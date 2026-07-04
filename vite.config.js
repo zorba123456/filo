592740a6aa244738ec94bfbf211cc96b5955817b
zorba123456 <123149337+zorba123456@users.noreply.github.com>
2026-07-04 10:36:52 +0800

    fix: use absolute paths for pwa icons in manifest

--- vite.config.js
+++ vite.config.js
@@ -16,22 +16,22 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-64x64.png',
+            src: '/filo/pwa-64x64.png',
             sizes: '64x64',
             type: 'image/png'
           },
           {
-            src: 'pwa-192x192.png',
+            src: '/filo/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/filo/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'maskable-icon-512x512.png',
+            src: '/filo/maskable-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
